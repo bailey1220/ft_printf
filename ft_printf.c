4ed21d8a28696db036608b38629a4194fa869791
Bailey Chai <bachai@u90z04s06.42kl.edu.my>
2024-07-25 15:15:33 +0800

    ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,17 +6,62 @@
 /*   By: bachai <bachai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 15:46:52 by bachai            #+#    #+#             */
-/*   Updated: 2024/07/18 17:36:56 by bachai           ###   ########.fr       */
+/*   Updated: 2024/07/25 15:14:21 by bachai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stdio.h>
+#include "libft.h"
+#include 
 
-int	ft_printf(const char *format, ...)
-(
-	va_list	args;
+int	check_type(const char *input, ...)
+{
 	int i;
+u
+	i = 0;
+	if (*input == 'c')
+		i += print_char((int)args);
+	if (*input == 's')
+		i += print_str((char)args);
+	if (*input == 'p')
+		i += print_ptr((unsigned char)args);
+	if (*input == 'd')
+		i += print_nbr((int)args);
+	if (*input == 'i')
+		i += print_nbr((int)args);
+	if (*input == 'u')
+		i += print_nbr((unsigned int)args);
+	if (*input == 'x')
+		i += print_hex((unsigned int)args);
+	if (*input == 'X')
+		i += print_hex((unsigned int)args);
+	return (i);
+}
 
-)
+int	ft_printf(const char *input, ...)
+{
+	int i;
+	va_list	args;
+
+	i = 0;
+	va_start(args, format);cspdiuxX%
+	while (*input != '\0')
+	{
+		if (*input == '%')
+		{
+			ft_strchr("cspdiuxX", input);
+			i =+ check_type(input, va_arg(args));
+		}
+		else if (*input == '%')
+			print_char('%');
+		
+
+	}
+			
+			
+
+
+
+
+
+
+}
--- print_hex.c
+++ print_hex.c
@@ -6,11 +6,104 @@
 /*   By: bachai <bachai@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 14:58:04 by bachai            #+#    #+#             */
-/*   Updated: 2024/07/21 17:51:38 by bachai           ###   ########.fr       */
+/*   Updated: 2024/07/25 15:06:26 by bachai           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "libft.h"
+
+int	hex_len(unsigned int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		num = num / 16;
+		len++;
+	}
+	return (len);
+}
+
+void	put_hex(unsigned int num, const char format)
+{
+	hex_char_lower[] = "0123456789abcdef";
+	hex_char_upper[] = "0123456789ABCDEF";
+	if (num >= 16)
+	{
+		put_hex(num / 16, format);
+		put_hex(num % 16, format);
+	}
+	else
+	{
+		if (num < 10)
+			ft_putchar_fd((num + '0'), 1);
+		else
+		{
+			if (format == 'x')
+				ft_putchar_fd(hex_char_lower[num], 1);
+			if (format == 'X')
+				ft_putchar_fd(hex_char_upper[num], 1);
+		}
+	}
+}
+
+int	print_hex(unsigned int num, const char format)
+{
+	if (num == 0)
+		return (write(1, "0", 1));
+	else
+		put_hex(num, format);
+	return (hex_len(num));
+}
+#include "ft_printf.h"
+#include "libft.h"
+
+int	hex_len(unsigned int num)
+{
+	int	len;
+
+	len = 0;
+	while (num != 0)
+	{
+		num = num / 16;
+		len++;
+	}
+	return (len);
+}
+
+void	put_hex(unsigned int num, const char format)
+{
+	hex_char_lower[] = "0123456789abcdef";
+	hex_char_upper[] = "0123456789ABCDEF";
+	if (num >= 16)
+	{
+		put_hex(num / 16, format);
+		put_hex(num % 16, format);
+	}
+	else
+	{
+		if (num < 10)
+			ft_putchar_fd((num + '0'), 1);
+		else
+		{
+			if (format == 'x')
+				ft_putchar_fd(hex_char_lower[num], 1);
+			if (format == 'X')
+				ft_putchar_fd(hex_char_upper[num], 1);
+		}
+	}
+}
+
+int	print_hex(unsigned int num, const char format)
+{
+	if (num == 0)
+		return (write(1, "0", 1));
+	else
+		put_hex(num, format);
+	return (hex_len(num));
+}include "ft_printf.h"
 #include "libft.h"
 
 int	hex_len(unsigned int num)
